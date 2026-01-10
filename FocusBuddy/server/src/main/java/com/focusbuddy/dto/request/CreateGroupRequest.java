9103dacdbd75b2de3b537c821ba5a8671a67ca77
CodeItAlone <subrato213432@gmail.com>
2026-01-11 04:57:53 +0530

    remove: delete CreateGroupRequest DTO

--- FocusBuddy/server/src/main/java/com/focusbuddy/dto/request/CreateGroupRequest.java
+++
@@ -1,9 +0,0 @@
-package com.focusbuddy.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record CreateGroupRequest(
-        @NotBlank(message = "Group name is required") String name,
-
-        @NotBlank(message = "Category is required") String category) {
-}
