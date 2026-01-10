eb96b9c8379a0da25a37f40533ded87dd2e66be6
CodeItAlone <subrato213432@gmail.com>
2026-01-11 04:56:01 +0530

    refactor: remove groups relationship from User model

--- FocusBuddy/server/src/main/java/com/focusbuddy/model/User.java
+++ FocusBuddy/server/src/main/java/com/focusbuddy/model/User.java
@@ -30,12 +30,5 @@ public class User {
     @EqualsAndHashCode.Exclude
     private Streak streak;
 
-    @ManyToMany(mappedBy = "members", fetch = FetchType.LAZY)
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
-    private java.util.List<Group> groups = new java.util.ArrayList<>();
-
     private LocalDateTime createdAt = LocalDateTime.now();
-
-    // Note: No sessions list here to avoid circular dependencies
 }
